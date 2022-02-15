43d518b10b84d2199cd4ea93d58b62a03f7abd7b
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2022-02-15 18:05:18 +0900

    確認用にaboutページを作成

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return HTMLResponse("<h1>Welcome! This is Root Page!</h1>")
+
+@app.get("/about")
+async def about():
+    return HTMLResponse("<h1>Welcome! This is About Page!</h1>")
