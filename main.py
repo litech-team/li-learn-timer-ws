ebbebf8021ba1ee001bd8537077065ad31710c55
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2022-02-19 16:24:27 +0900

    chatのテストページを削除

--- main.py
+++ main.py
@@ -20,46 +20,10 @@ events = Events()
 
 app = FastAPI()
 
-html = r"""
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var data = JSON.parse(event.data)
-                var text = data.props?.text ?? "successful!"
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(value)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(JSON.stringify({value: input.value}))
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
 
 @app.get("/")
 async def get():
-    return HTMLResponse(html)
+    return HTMLResponse(r"<h1>Hello World</h1>")
 
 
 @app.websocket("/ws")
