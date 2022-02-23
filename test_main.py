ad62834fb43e125a1f43f677dbbc630991e6e575
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2022-02-23 21:37:28 +0900

    テストファイルを作成

---
+++ test_main.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+
+from main import app
+
+def test_ws_raspberry_pi_1():
+    client = TestClient(app)
+
+    with client.websocket_connect("/raspberry-pi") as websocket:
+        websocket.send_json({
+            "name": "send_pi_id",
+            "props": {
+                "pi_id": "01234"
+            }
+        })
+        assert websocket.receive_json() == {
+            "name": "ack"
+        }
